9488a2ce1505114be172675470184eb67e1b587f
Mike Wright <mike.wright@itsallwater.net>
2019-02-24 13:16:42 -0700

    - Next-Level Vue #9

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -41,7 +41,7 @@
         v-model="event.time"
         class="field"
       />
-      <input type="submit" class="button -fill-gradient" value="Submit" />
+      <BaseButton type="submit" buttonClass="-fill-gradient">Submit</BaseButton>
     </form>
   </div>
 </template>
