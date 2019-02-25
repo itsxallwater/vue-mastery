66943a084136560bbca7f537f63db9f832f2baf2
Mike Wright <mike.wright@itsallwater.net>
2019-02-24 22:25:27 -0700

    - Next-Level Vue #12

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -11,26 +11,16 @@
 </template>
 
 <script>
+import { formFieldMixin } from '../mixins/formFieldMixin.js'
+
 export default {
-  inheritAttrs: false,
-  props: {
-    label: {
-      type: String,
-      default: ''
-    },
-    value: [String, Number]
-  },
+  mixins: [formFieldMixin],
   computed: {
     listeners() {
       return {
         ...this.$listeners,
         input: this.updateValue
       }
-    }
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
     }
   }
 }
--- src/components/BaseSelect.vue
+++ src/components/BaseSelect.vue
@@ -19,22 +19,13 @@
 </template>
 
 <script>
+import { formFieldMixin } from '../mixins/formFieldMixin.js'
 export default {
-  inheritAttrs: false,
+  mixins: [formFieldMixin],
   props: {
     options: {
       type: Array,
       required: true
-    },
-    label: {
-      type: String,
-      default: ''
-    },
-    value: [String, Number]
-  },
-  methods: {
-    updateValue(event) {
-      this.$emit('input', event.target.value)
     }
   }
 }
