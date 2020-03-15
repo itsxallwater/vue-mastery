23300215f544801923b6cc96004df5ff1975b5b6
Mike Wright <mikew@zumasys.com>
2020-03-15 14:28:03 -0700

    Token Based Authentication - Lesson 4, User Login

--- token-based-authentication/src/views/LoginUser.vue
+++ token-based-authentication/src/views/LoginUser.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <form @submit.prevent="register">
-      <label for="name">
-        Name:
-      </label>
-      <input v-model="name" type="text" name="name" value />
-
+    <form @submit.prevent="login">
       <label for="email">
         Email:
       </label>
@@ -14,13 +9,13 @@
       <label for="password">
         Password:
       </label>
-      <input v-model="password" type="password" name value />
+      <input v-model="password" type="password" name="password" value />
 
       <button type="submit" name="button">
-        Register
+        Login
       </button>
-      <router-link to="/login">
-        Already have an account? Login.
+      <router-link to="/register">
+        Don't have an account? Register.
       </router-link>
     </form>
   </div>
@@ -30,16 +25,14 @@
 export default {
   data() {
     return {
-      name: '',
       email: '',
       password: ''
     }
   },
   methods: {
-    register() {
+    login() {
       this.$store
-        .dispatch('register', {
-          name: this.name,
+        .dispatch('login', {
           email: this.email,
           password: this.password
         })
