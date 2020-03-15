53ac7bb46ecb7eff39360859ec04d772764f8a09
Mike Wright <mikew@zumasys.com>
2020-03-15 15:02:27 -0700

    Init Unit Testing

---
+++ unit-testing/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <app-header />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+button {
+  height: 30px;
+  width: 60px;
+  background: linear-gradient(to right, #16c0b0, #84cf6a);
+  text-decoration: none;
+  color: white;
+  border-radius: 3px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
